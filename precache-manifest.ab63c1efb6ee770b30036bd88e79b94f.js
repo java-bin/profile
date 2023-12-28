5e69b92bcb22454f0dc168fe9a65e62a0552f1c0
BIN <55619475+JAVA-BIN@users.noreply.github.com>
2023-12-28 23:33:49 +0900

    Updates

---
+++ precache-manifest.ab63c1efb6ee770b30036bd88e79b94f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c337aa1d849bac09d5e3d8fcb1f3fdc1",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "269ec20711a14ff4bca8",
+    "url": "/profile/static/css/main.1d7c5dd8.chunk.css"
+  },
+  {
+    "revision": "a65bd153d334dd242d60",
+    "url": "/profile/static/js/2.b33575a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/profile/static/js/2.b33575a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "269ec20711a14ff4bca8",
+    "url": "/profile/static/js/main.d8bcb23b.chunk.js"
+  },
+  {
+    "revision": "fd76696900ebdbcba316",
+    "url": "/profile/static/js/runtime-main.2a118a22.js"
+  },
+  {
+    "revision": "a4036cc9e96c2f5da3ff30a030efb261",
+    "url": "/profile/static/media/bin.a4036cc9.jpeg"
+  },
+  {
+    "revision": "14e135a1d003862892e0ed98c8da2b13",
+    "url": "/profile/static/media/korea.14e135a1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/profile/precache-manifest.dd5a61b5ee7349e6ab04c99d282407f6.js"
+  "/profile/precache-manifest.ab63c1efb6ee770b30036bd88e79b94f.js"
 );
 
 self.addEventListener('message', (event) => {
